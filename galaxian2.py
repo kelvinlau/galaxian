nn3d: Fix 0 gradient of relu. fix 0 q target. Refactor common part of Frame2 and Frame.

galaxian2.py
@@ -6,6 +6,7 @@ Ref: https://www.nervanasys.com/demystifying-deep-reinforcement-learning/
 
 TODO: Save png to verify input data.
 TODO: Scale down the image by 2x.
+TODO: Sigmoid vs ReLu.
 """
 
 from __future__ import print_function
@@ -14,13 +15,22 @@ from collections import deque
 import os
 import random
 import time
+import math
 import socket
 import numpy as np
 import tensorflow as tf
 
 
-WIDTH = 256
-HEIGHT = 240
+RAW_IMAGE = False
+if RAW_IMAGE:
+  WIDTH = 256
+  HEIGHT = 240
+else:
+  # galaxian.x, missile.y, still enemy xs, 6 incoming enemies and 6 bullets.
+  # TODO: Try adding if the galaxian is aimming at a gap.
+  INPUT_DIM = 1 + 1 + 10 + 2 * (6 + 6)
+  # INPUT_DIM = 1 + 1 + 2 * (1 + 2)
+
 NUM_SNAPSHOTS = 5
 ACTION_NAMES = ['_', 'L', 'R', 'A']
 ACTION_ID = {'_': 0, 'L': 1, 'R': 2, 'A': 3}
@@ -47,7 +57,7 @@ class Point:
     self.y = y
 
 
-class Frame2:
+class Frame:
   def __init__(self, line):
     """Parse a Frame from a line."""
     self._tokens = line.split()
@@ -55,60 +65,134 @@ class Frame2:
 
     self.seq = self.NextInt()
 
-    self.reward = self.NextInt()
+    self.reward = max(0, self.NextInt() + 1)
 
     self.action = self.NextToken()
     self.action_id = ACTION_ID[self.action]
 
-    self.image = np.zeros((WIDTH, HEIGHT))
-
     galaxian = self.NextPoint()
-    self.AddRect(galaxian, 16, 16)
 
     missile = self.NextPoint()
-    if missile.y < 200:
-      self.AddRect(missile, 4, 12)
 
-    still_enemies = []
+    # still enemies (encoded)
     dx = self.NextInt()
+    masks = []
     for i in xrange(10):
-      x = (dx + 48 + 8 + 16 * i) % 256;
-      y = 108
-      mask = self.NextInt()
-      while mask:
-        if mask % 2:
-          still_enemies.append(Point(x, y))
-        mask /= 2
-        y -= 12
-    assert len(still_enemies) <= 46
-    for e in still_enemies:
-      self.AddRect(e, 10, 12)
+      masks.append(self.NextInt())
 
     incoming_enemies = []
     for i in xrange(self.NextInt()):
       incoming_enemies.append(self.NextPoint())
-    for e in incoming_enemies:
-      self.AddRect(e, 10, 12)
 
     bullets = []
     for i in xrange(self.NextInt()):
       bullets.append(self.NextPoint())
-    for b in bullets:
-      self.AddRect(b, 4, 12)
+
+    if not RAW_IMAGE:
+      data = []
+
+      data.append((galaxian.x - 128) / 128.0)
+
+      if missile.y < 200:
+        data.append(missile.y / 200.0)
+      else:
+        data.append(0)
+
+      # still_enemies
+      still_enemy_xs = []
+      for mask in masks:
+        x = (dx + 48 + 8 + 16 * i) % 256;
+        if mask:
+          still_enemy_xs.append(Frame.InvertX(x - galaxian.x))
+      still_enemy_xs.sort(key = abs, reverse = True)
+      for x in still_enemy_xs:
+        data.append(x)
+      for i in xrange(10 - len(still_enemy_xs)):
+        data.append(0)
+
+      for e in incoming_enemies:
+        dx = Frame.InvertX(e.x - galaxian.x)
+        dy = e.y / 200.0
+        data.append(dx)
+        data.append(dy)
+      for i in xrange(6 - len(incoming_enemies)):
+        data.append(0)
+        data.append(0)
+
+      for e in bullets:
+        dx = Frame.InvertX(e.x - galaxian.x)
+        dy = e.y / 200.0
+        data.append(dx)
+        data.append(dy)
+      for i in xrange(6 - len(bullets)):
+        data.append(0)
+        data.append(0)
+
+      assert len(data) == INPUT_DIM
+      self.data = np.array(data)
+    else:
+      self.data = np.zeros((WIDTH, HEIGHT))
+
+      self.AddRect(galaxian, 16, 16)
+
+      if missile.y < 200:
+        self.AddRect(missile, 4, 12)
+
+      still_enemies = []
+      for mask in masks:
+        x = (dx + 48 + 8 + 16 * i) % 256;
+        y = 108
+        while mask:
+          if mask % 2:
+            still_enemies.append(Point(x, y))
+          mask /= 2
+          y -= 12
+      assert len(still_enemies) <= 46
+      for e in still_enemies:
+        self.AddRect(e, 10, 12)
+
+      for e in incoming_enemies:
+        self.AddRect(e, 10, 12)
+
+      for b in bullets:
+        self.AddRect(b, 4, 12)
+
+  @staticmethod
+  def InvertX(dx):
+    if dx > 0:
+      return (256 - dx) / 256.0
+    elif dx < 0:
+      return (-256 - dx) / 256.0
+    else:
+      return 1
 
   def AddSnapshotsFromSelf(self):
-    self.imagex = np.reshape(self.image, (WIDTH, HEIGHT, 1))
-    for i in xrange(NUM_SNAPSHOTS-1):
-      self.imagex = np.append(
-          self.imagex,
-          np.reshape(self.image, (WIDTH, HEIGHT, 1)),
-          axis = 2)
+    if not RAW_IMAGE:
+      self.datax = np.reshape(self.data, (INPUT_DIM, 1))
+      for i in xrange(NUM_SNAPSHOTS-1):
+        self.datax = np.append(
+            self.datax,
+            np.reshape(self.data, (INPUT_DIM, 1)),
+            axis = 1)
+    else:
+      self.datax = np.reshape(self.data, (WIDTH, HEIGHT, 1))
+      for i in xrange(NUM_SNAPSHOTS-1):
+        self.datax = np.append(
+            self.datax,
+            np.reshape(self.data, (WIDTH, HEIGHT, 1)),
+            axis = 2)
 
   def AddSnapshotsFromPrev(self, prev_frame):
-    self.imagex = np.append(
-        np.reshape(self.image, (WIDTH, HEIGHT, 1)),
-        prev_frame.imagex[:, :, :NUM_SNAPSHOTS-1],
-        axis = 2)
+    if not RAW_IMAGE:
+      self.datax = np.append(
+          np.reshape(self.data, (INPUT_DIM, 1)),
+          prev_frame.datax[:, :NUM_SNAPSHOTS-1],
+          axis = 1)
+    else:
+      self.datax = np.append(
+          np.reshape(self.data, (WIDTH, HEIGHT, 1)),
+          prev_frame.datax[:, :, :NUM_SNAPSHOTS-1],
+          axis = 2)
 
   def NextToken(self):
     self._idx += 1
@@ -129,103 +213,7 @@ class Frame2:
     y2 = min(c.y + h, HEIGHT)
     if x1 >= x2 or y1 >= y2:
       return
-    self.image[x1:x2, y1:y2] += np.full((x2-x1, y2-y1,), 1.)
-
-
-class Frame:
-  # galaxian.x, missile.y, still enemies dx and 10 masks, 6 incoming enemies and
-  # 6 bullets.
-  # TODO: Try removing the 11.
-  # TODO: Try adding if the galaxian is aimming at a gap.
-  INPUT_DIM = 1 + 1 + 11 + 2 * (6 + 6)
-
-  def __init__(self, line):
-    """Parse a Frame from a line."""
-    self._tokens = line.split()
-    self._idx = 0
-
-    self.seq = self.NextInt()
-
-    self.reward = max(0, self.NextInt() + 1)
-
-    self.action = self.NextToken()
-    self.action_id = ACTION_ID[self.action]
-
-    data = []
-
-    galaxian = self.NextPoint()
-    data.append((galaxian.x - 128) / 128.0)
-
-    missile = self.NextPoint()
-    if missile.y < 200:
-      data.append(missile.y / 200.0)
-    else:
-      data.append(0)
-
-    # still_enemies
-    dx = self.NextInt()
-    data.append((dx - galaxian.x) / 256.0)
-    for i in xrange(10):
-      mask = self.NextInt()
-      data.append(1 if mask else 0)
-
-    num_incoming_enemies = self.NextInt()
-    for i in xrange(num_incoming_enemies):
-      e = self.NextPoint()
-      dx = Frame.InvertX(e.x - galaxian.x)
-      dy = e.y / 200.0
-      data.append(dx)
-      data.append(dy)
-    for i in xrange(6 - num_incoming_enemies):
-      data.append(0)
-      data.append(0)
-
-    num_bullets = self.NextInt()
-    for i in xrange(num_bullets):
-      e = self.NextPoint()
-      dx = Frame.InvertX(e.x - galaxian.x)
-      dy = e.y / 200.0
-      data.append(dx)
-      data.append(dy)
-    for i in xrange(6 - num_bullets):
-      data.append(0)
-      data.append(0)
-
-    assert len(data) == self.INPUT_DIM
-    self.data = np.array(data)
-
-  @staticmethod
-  def InvertX(dx):
-    if dx > 0:
-      return (256 - dx) / 256.0
-    elif dx < 0:
-      return (-256 - dx) / 256.0
-    else:
-      return 1
-
-  def AddSnapshotsFromSelf(self):
-    self.datax = np.reshape(self.data, (self.INPUT_DIM, 1))
-    for i in xrange(NUM_SNAPSHOTS-1):
-      self.datax = np.append(
-          self.datax,
-          np.reshape(self.data, (self.INPUT_DIM, 1)),
-          axis = 1)
-
-  def AddSnapshotsFromPrev(self, prev_frame):
-    self.datax = np.append(
-        np.reshape(self.data, (self.INPUT_DIM, 1)),
-        prev_frame.datax[:, :NUM_SNAPSHOTS-1],
-        axis = 1)
-
-  def NextToken(self):
-    self._idx += 1
-    return self._tokens[self._idx - 1]
-
-  def NextInt(self):
-    return int(self.NextToken())
-
-  def NextPoint(self):
-    return Point(self.NextInt(), self.NextInt())
+    self.data[x1:x2, y1:y2] += np.full((x2-x1, y2-y1,), 1.)
 
   def CheckSum(self):
     return np.sum(self.datax)
@@ -251,8 +239,6 @@ class Game:
 
     assert frame.seq == self._seq, 'Expecting %d, got %d' % (self._seq,
         frame.seq)
-    # assert frame.action == action, 'Expecting %s, got %s' % (action,
-    #     frame.action)
 
     return frame
 
@@ -275,102 +261,70 @@ def TestGame():
       game.Step('_')
 
 
-class NeuralNetwork2:
-  def __init__(self):
-    # Input image.
-    self.input = tf.placeholder(tf.float32,
-        [None, WIDTH, HEIGHT, NUM_SNAPSHOTS])
-    print('input:', self.input.get_shape())
-
-    zeros = lambda shape: tf.Variable(tf.zeros(shape))
-
-    # Conv 1: 16x12 is the enemy size.
-    conv1 = tf.nn.relu(tf.nn.conv2d(
-      self.input, zeros([32, 24, 5, 64]), strides = [1, 16, 12, 1],
-      padding = "VALID")
-      + zeros([64]))
-    print('conv1:', conv1.get_shape())
-
-    # Conv 2.
-    conv2 = tf.nn.relu(tf.nn.conv2d(
-      conv1, zeros([4, 4, 64, 128]), strides = [1, 2, 2, 1],
-      padding = "VALID")
-      + zeros([128]))
-    print('conv2:', conv2.get_shape())
-
-    # Conv 3.
-    conv3 = tf.nn.relu(tf.nn.conv2d(
-      conv2, zeros([2, 2, 128, 128]), strides = [1, 1, 1, 1],
-      padding = "VALID")
-      + zeros([128]))
-    print('conv3:', conv3.get_shape())
-
-    # Flatten conv 3.
-    conv3_flat = tf.reshape(conv3, [-1, 4480])
-
-    # Full connected 4.
-    fc4 = tf.nn.relu(tf.matmul(conv3_flat, zeros([4480, 784])) + zeros([784]))
-
-    # Output.
-    self.output_layer = (tf.matmul(fc4, zeros([784, OUTPUT_DIM])) +
-        zeros([OUTPUT_DIM]))
-
-    # Training.
-    self.action = tf.placeholder(tf.float32, [None, OUTPUT_DIM])
-    self.q_target = tf.placeholder(tf.float32, [None])
-    q_action = tf.reduce_sum(tf.mul(self.output_layer, self.action),
-        reduction_indices = 1)
-    cost = tf.reduce_mean(tf.square(q_action - self.q_target))
-    self.optimizer = tf.train.AdamOptimizer(
-        learning_rate=1e-2, epsilon=1e-3).minimize(cost)
-
-  def Eval(self, frames):
-    return self.output_layer.eval(feed_dict = {
-        self.input: [f.imagex for f in frames]
-    })
-
-  def Train(self, mini_batch):
-    frame_batch = [d[0] for d in mini_batch]
-    action_batch = [d[1] for d in mini_batch]
-    frame1_batch = [d[2] for d in mini_batch]
-
-    q1_batch = self.Eval(frame1_batch)
-    q_target_batch = [
-        frame1_batch[i].reward if frame1_batch[i].reward <= 0 else
-        frame1_batch[i].reward + GAMMA * np.max(q1_batch[i])
-        for i in xrange(len(mini_batch))
-    ]
-
-    self.optimizer.run(feed_dict = {
-        self.input: [f.imagex for f in frame_batch],
-        self.action: action_batch,
-        self.q_target: q_target_batch,
-    })
-
-
 class NeuralNetwork:
   def __init__(self):
-    # Input.
-    self.input = tf.placeholder(tf.float32,
-        [None, Frame.INPUT_DIM, NUM_SNAPSHOTS])
-    print('input:', self.input.get_shape())
+    var = lambda shape: tf.Variable(tf.random_normal(shape))
 
-    zeros = lambda shape: tf.Variable(tf.zeros(shape))
+    if not RAW_IMAGE:
+      # Input.
+      self.input = tf.placeholder(tf.float32,
+          [None, INPUT_DIM, NUM_SNAPSHOTS])
+      print('input:', self.input.get_shape())
 
-    # Flatten input.
-    INPUT_FLAT_DIM = Frame.INPUT_DIM * NUM_SNAPSHOTS
-    input_flat = tf.reshape(self.input, [-1, INPUT_FLAT_DIM])
+      # Flatten input.
+      INPUT_FLAT_DIM = INPUT_DIM * NUM_SNAPSHOTS
+      input_flat = tf.reshape(self.input, [-1, INPUT_FLAT_DIM])
 
-    # Full connected 1.
-    fc1 = tf.nn.relu(tf.matmul(input_flat, zeros([INPUT_FLAT_DIM, 128])) +
-        zeros([128]))
+      # Full connected 1.
+      self.w1 = var([INPUT_FLAT_DIM, 16])
+      self.b1 = var([16])
+      fc1 = tf.nn.sigmoid(tf.matmul(input_flat, self.w1) + self.b1)
 
-    # Full connected 2.
-    fc2 = tf.nn.relu(tf.matmul(fc1, zeros([128, 64])) + zeros([64]))
+      # Full connected 2.
+      self.w2 = var([16, 8])
+      self.b2 = var([8])
+      fc2 = tf.nn.sigmoid(tf.matmul(fc1, self.w2) + self.b2)
 
-    # Output.
-    self.output_layer = (tf.matmul(fc2, zeros([64, OUTPUT_DIM])) +
-        zeros([OUTPUT_DIM]))
+      # Output.
+      self.w3 = var([8, OUTPUT_DIM])
+      self.b3 = var([OUTPUT_DIM])
+      self.output_layer = (tf.matmul(fc2, self.w3) + self.b3)
+    else:
+      # Input image.
+      self.input = tf.placeholder(tf.float32,
+          [None, WIDTH, HEIGHT, NUM_SNAPSHOTS])
+      print('input:', self.input.get_shape())
+
+      # Conv 1: 16x12 is the enemy size.
+      conv1 = tf.nn.relu(tf.nn.conv2d(
+        self.input, var([32, 24, 5, 64]), strides = [1, 16, 12, 1],
+        padding = "VALID")
+        + var([64]))
+      print('conv1:', conv1.get_shape())
+
+      # Conv 2.
+      conv2 = tf.nn.relu(tf.nn.conv2d(
+        conv1, var([4, 4, 64, 128]), strides = [1, 2, 2, 1],
+        padding = "VALID")
+        + var([128]))
+      print('conv2:', conv2.get_shape())
+
+      # Conv 3.
+      conv3 = tf.nn.relu(tf.nn.conv2d(
+        conv2, var([2, 2, 128, 128]), strides = [1, 1, 1, 1],
+        padding = "VALID")
+        + var([128]))
+      print('conv3:', conv3.get_shape())
+
+      # Flatten conv 3.
+      conv3_flat = tf.reshape(conv3, [-1, 4480])
+
+      # Full connected 4.
+      fc4 = tf.nn.relu(tf.matmul(conv3_flat, var([4480, 784])) + var([784]))
+
+      # Output.
+      self.output_layer = (tf.matmul(fc4, var([784, OUTPUT_DIM])) +
+          var([OUTPUT_DIM]))
 
     # Training.
     self.action = tf.placeholder(tf.float32, [None, OUTPUT_DIM])
@@ -393,7 +347,7 @@ class NeuralNetwork:
 
     q1_batch = self.Eval(frame1_batch)
     q_target_batch = [
-        frame1_batch[i].reward if frame1_batch[i].reward < 0 else
+        frame1_batch[i].reward if frame1_batch[i].reward <= 0 else
         frame1_batch[i].reward + GAMMA * np.max(q1_batch[i])
         for i in xrange(len(mini_batch))
     ]
@@ -404,20 +358,30 @@ class NeuralNetwork:
         self.q_target: q_target_batch,
     }
     self.optimizer.run(feed_dict = feed_dict)
-    return self.cost.eval(feed_dict = feed_dict)
+    return self.cost.eval(feed_dict = feed_dict), q_target_batch[-1]
+
+  def CheckSums(self):
+    return (
+        np.sum(self.w1.eval()),
+        np.sum(self.b1.eval()),
+        np.sum(self.w2.eval()),
+        np.sum(self.b2.eval()),
+        np.sum(self.w3.eval()),
+        np.sum(self.b3.eval()),
+        )
 
 
-# Deep Learning Params
+# Deep Learning params
 GAMMA = 0.99
-INITIAL_EPSILON = 0.5 # 1.0
+INITIAL_EPSILON = 0.1 # 1.0
 FINAL_EPSILON = 0.05 # 0.05
 EXPLORE_STEPS = 500000
 OBSERVE_STEPS = 0 # 10000
 REPLAY_MEMORY = 10000 # 2000  # ~6G memory
 MINI_BATCH_SIZE = 100
-TRAIN_INTERVAL = 10  # 24
+TRAIN_INTERVAL = 1
 
-CHECKPOINT_DIR = 'galaxian3b/'
+CHECKPOINT_DIR = 'galaxian2/'
 CHECKPOINT_FILE = 'model.ckpt'
 SAVE_INTERVAL = 1000 # 10000
 
@@ -443,9 +407,10 @@ def Run():
     steps = 0
     epsilon = INITIAL_EPSILON
     cost = 1e9
+    q_target_val = -1
     while True:
       if random.random() <= epsilon:
-        q_val = None
+        q_val = []
         action = ACTION_NAMES[random.randrange(OUTPUT_DIM)]
       else:
         q_val = nn.Eval([frame])[0]
@@ -465,9 +430,7 @@ def Run():
       if steps % TRAIN_INTERVAL == 0 and steps > OBSERVE_STEPS:
         mini_batch = random.sample(memory, min(len(memory), MINI_BATCH_SIZE))
         mini_batch.append(memory[-1])
-        cost = nn.Train(mini_batch)
-
-      qx_val = nn.Eval([frame])[0]
+        cost, q_target_val = nn.Train(mini_batch)
 
       frame = frame1
       steps += 1
@@ -479,12 +442,10 @@ def Run():
                                global_step = steps)
         print("Saved to", save_path)
 
-      print("Step %7d epsilon: %.6f action: %s reward: %4.0f q: %-40s "
-          "qx: %-40s cost: %8.2f" %
-          (steps, epsilon, frame1.action, frame1.reward,
-            ' '.join(map(str, q_val)) if q_val is not None else '',
-            ' '.join(map(str, qx_val)) if qx_val is not None else '',
-            cost))
+      print("Step %d epsilon: %.6f nn: %s action: %s reward: %3.0f "
+          "q: %-32s cost: %8.2f q_target: %8.2f" %
+          (steps, epsilon, nn.CheckSums(), frame1.action, frame1.reward,
+            ' '.join(map(lambda v : '%7.2f' % v, q_val)), cost, q_target_val))
 
 
 if __name__ == '__main__':